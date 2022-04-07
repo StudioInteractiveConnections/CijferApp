2b1575682b1fc46b4a2be59b0a197896f5a3f852
StudioInteractiveConnections <studio.interactive.connections@gmail.com>
2022-04-07 12:33:07 +0200

    Player can pickup Options 1-3
    The play can now pick up 3 objects

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject player;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.collider.name)
+        {
+            case "Option1":
+                Debug.Log("Touching Option1");
+                other.transform.SetParent(player.transform);
+                other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+                break;
+           
+            case "Option2":
+                Debug.Log("Touching Option1");
+                other.transform.SetParent(player.transform);
+                other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+                break;
+           
+            case "Option3":
+                Debug.Log("Touching Option1");
+                other.transform.SetParent(player.transform);
+                other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+                break;
+
+            default:
+                Debug.Log("Not touching an option");
+                break;
+        }
+       
+    }
+}
